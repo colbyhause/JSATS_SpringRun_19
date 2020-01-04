155f0a7efd51a8fb12ee52b7bb488c3019534739
Colby Hause <chause@ucdavis.edu>
2020-01-03 16:07:06 -0800

    ran 010320 data through code

--- script/Make_Conditional_Counts_w_Aux_Delta2019.R
+++ script/Make_Conditional_Counts_w_Aux_Delta2019.R
@@ -11,7 +11,9 @@ library(tidyverse)
 # Make Detection Counts ---------------------------------------------------
 
 # Load UPPER RELEASE dataframe with just first visits ---------------------------------------------------------------
-data <- read_csv("data_output/DetectionFiles/Files_w_Bridge_Data/FULLmodel_final/Full_Model_Edited/DeltaRel_visits2019_first_All_NOAAdata_120319.csv")
+#data <- read_csv("data_output/DetectionFiles/Files_w_Bridge_Data/FULLmodel_final/Full_Model_Edited/DeltaRel_visits2019_first_All_NOAAdata_120319.csv")
+data <- read_csv("data_output/DetectionFiles/Files_w_Bridge_Data/FULLmodel_final/Full_Model_Edited/DeltaRel_visits2019_first_All_NOAAdata_010320.csv")
+
 unique(data$`GPS Names`)
 # Make a vector of locations that are included in model 
 
@@ -91,7 +93,9 @@ for(i in 1: nrow(receivers)){ # interating through each row in the receivers df 
 }
 totals
 input <- data.frame(cbind(receivers, totals)) # Conditional likelihoods,  auxiliary ones for dual arrays are below
-write.csv(input, 'data_output/User_Model_Input/2019_FullModel/2019_DeltaRelease_counts_FullModel_AllNOAAdata_FINAL_120319.csv')
+#write.csv(input, 'data_output/User_Model_Input/2019_FullModel/2019_DeltaRelease_counts_FullModel_AllNOAAdata_FINAL_120319.csv')
+write.csv(input, 'data_output/User_Model_Input/2019_FullModel/2019_DeltaRelease_counts_FullModel_AllNOAAdata_FINAL_010320.csv')
+
 
 # ><)))))*>  ------------- ><)))))*>  ------------- ><)))))*>  ------------- ><)))))*>  ------------- ><)))))*>  
 
@@ -461,7 +465,8 @@ Aux <- rbind(t(ORHOR_dual),t(MRHWY4_dual), t(CC_dual),t(MAC_dual),t(CVP_dual),
              t(JP_dual), t(Chipps_dual), t(GG_dual), t(Ben_dual))
 # t() function transposes the data, ie. makes the columns the rows 
 
-write.csv(Aux, "data_output/User_Model_Input/2019_FullModel/2019_DeltaRel_Cond_Aux_counts_FullModel_AllNOAAdata_FINAL_120319.csv")
+#write.csv(Aux, "data_output/User_Model_Input/2019_FullModel/2019_DeltaRel_Cond_Aux_counts_FullModel_AllNOAAdata_FINAL_120319.csv")
+write.csv(Aux, "data_output/User_Model_Input/2019_FullModel/2019_DeltaRel_Cond_Aux_counts_FullModel_AllNOAAdata_FINAL_010320.csv")
 
 
 
